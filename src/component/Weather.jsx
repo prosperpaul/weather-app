05db8e4dfaa4a300a622a50a059670f6061dadcb
paul <amakapaul4cbn@gmail.com>
2025-08-10 19:19:29 -1200

    fix: correct API_KEYS

--- src/component/Weather.jsx
+++ src/component/Weather.jsx
@@ -38,6 +38,7 @@ const Weather = () => {
     return;
   };
    try{
+    
       const url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_APP_ID}`;
 
       const response = await fetch(url);
